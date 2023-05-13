d06ca641d3174e462422af31a41c94a62cecafbe
BradDWagner <braddwagner@outlook.com>
2023-05-13 16:46:52 -0700

    fixed issue with Utility context

--- Pages/Departments/Edit.cshtml.cs
+++ Pages/Departments/Edit.cshtml.cs
@@ -1,13 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using ContosoUniversity.Data;
-using ContosoUniversity.Models;
+using System;                // For GUID in SQLite version
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ContosoUniversity.Pages.Departments
 {
@@ -22,14 +21,14 @@ namespace ContosoUniversity.Pages.Departments
 
         [BindProperty]
         public Department Department { get; set; }
-        
-        public SelectList InstructorNameSL {get; set; }
+        // Replace ViewData["InstructorID"] 
+        public SelectList InstructorNameSL { get; set; }
 
-        public async Task<IActionResult> OnGetAsync(int? id)
+        public async Task<IActionResult> OnGetAsync(int id)
         {
             Department = await _context.Departments
-                .Include(d => d.Administrator)
-                .AsNoTracking()
+                .Include(d => d.Administrator)  // eager loading
+                .AsNoTracking()                 // tracking not required
                 .FirstOrDefaultAsync(m => m.DepartmentID == id);
 
             if (Department == null)
@@ -37,13 +36,13 @@ namespace ContosoUniversity.Pages.Departments
                 return NotFound();
             }
 
-            InstructorNameSL = new SelectList(_context.Instructors, "ID", "FirstMidName");
+            // Use strongly typed data rather than ViewData.
+            InstructorNameSL = new SelectList(_context.Instructors,
+                "ID", "FirstMidName");
 
             return Page();
         }
 
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync(int id)
         {
             if (!ModelState.IsValid)
@@ -52,7 +51,7 @@ namespace ContosoUniversity.Pages.Departments
             }
 
             // Fetch current department from DB.
-            // ConcurrencyToken may have changed
+            // ConcurrencyToken may have changed.
             var departmentToUpdate = await _context.Departments
                 .Include(i => i.Administrator)
                 .FirstOrDefaultAsync(m => m.DepartmentID == id);
@@ -65,7 +64,8 @@ namespace ContosoUniversity.Pages.Departments
             departmentToUpdate.ConcurrencyToken = Guid.NewGuid();
 
             // Set ConcurrencyToken to value read in OnGetAsync
-            _context.Entry(departmentToUpdate).Property(d => d.ConcurrencyToken).OriginalValue = Department.ConcurrencyToken;
+            _context.Entry(departmentToUpdate).Property(d => d.ConcurrencyToken)
+                                   .OriginalValue = Department.ConcurrencyToken;
 
             if (await TryUpdateModelAsync<Department>(
                 departmentToUpdate,
@@ -84,65 +84,71 @@ namespace ContosoUniversity.Pages.Departments
                     var databaseEntry = exceptionEntry.GetDatabaseValues();
                     if (databaseEntry == null)
                     {
-                        ModelState.AddModelError(string.Empty, "Unable to save. " + "The Department was deleted by another user.");
+                        ModelState.AddModelError(string.Empty, "Unable to save. " +
+                            "The department was deleted by another user.");
                         return Page();
                     }
 
                     var dbValues = (Department)databaseEntry.ToObject();
                     await SetDbErrorMessage(dbValues, clientValues, _context);
 
-                    // Save the curent ConcurrencyToekn so next prostback matches unless a new concurrency issue happens.
+                    // Save the current ConcurrencyToken so next postback
+                    // matches unless an new concurrency issue happens.
                     Department.ConcurrencyToken = dbValues.ConcurrencyToken;
                     // Clear the model error for the next postback.
                     ModelState.Remove($"{nameof(Department)}.{nameof(Department.ConcurrencyToken)}");
                 }
             }
 
-            InstructorNameSL = new SelectList(_context.Instructors, "ID", "FullName", departmentToUpdate.InstructorID);
+            InstructorNameSL = new SelectList(_context.Instructors,
+                "ID", "FullName", departmentToUpdate.InstructorID);
+
             return Page();
         }
 
         private IActionResult HandleDeletedDepartment()
         {
-            //ModelState contains the posted data because of the deletion error
-            //and overides the Department instance values when displaying Page().
-            ModelState.AddModelError(string.Empty, "Unable to save. The department was deleted by another user.");
+            // ModelState contains the posted data because of the deletion error
+            // and overides the Department instance values when displaying Page().
+            ModelState.AddModelError(string.Empty,
+                "Unable to save. The department was deleted by another user.");
             InstructorNameSL = new SelectList(_context.Instructors, "ID", "FullName", Department.InstructorID);
             return Page();
         }
 
-        private async Task SetDbErrorMessage(Department dbValues, Department clientValues, SchoolContext context)
+        private async Task SetDbErrorMessage(Department dbValues,
+                Department clientValues, SchoolContext context)
         {
+
             if (dbValues.Name != clientValues.Name)
             {
-                ModelState.AddModelError("Department.Name", $"Current value: {dbValues.Name}");
+                ModelState.AddModelError("Department.Name",
+                    $"Current value: {dbValues.Name}");
             }
             if (dbValues.Budget != clientValues.Budget)
             {
-                ModelState.AddModelError("Department.Budget", $"CurrentValue: {dbValues.Budget:c}");
+                ModelState.AddModelError("Department.Budget",
+                    $"Current value: {dbValues.Budget:c}");
             }
             if (dbValues.StartDate != clientValues.StartDate)
             {
-                ModelState.AddModelError("Department.StartDate", $"Current value: {dbValues.StartDate:d}");
+                ModelState.AddModelError("Department.StartDate",
+                    $"Current value: {dbValues.StartDate:d}");
             }
             if (dbValues.InstructorID != clientValues.InstructorID)
             {
-                Instructor dbInstructor = await _context.Instructors 
-                    .FindAsync(dbValues.InstructorID);
-                ModelState.AddModelError("Department.InstructorID", $"Current value: {dbInstructor?.FullName}");
+                Instructor dbInstructor = await _context.Instructors
+                   .FindAsync(dbValues.InstructorID);
+                ModelState.AddModelError("Department.InstructorID",
+                    $"Current value: {dbInstructor?.FullName}");
             }
 
             ModelState.AddModelError(string.Empty,
                 "The record you attempted to edit "
-                + "was modified by another user after you. The "
-                + "edit operation was canceled and the current values in the database "
-                + "have been displayed. If you still want to edit this record, click "
-                + "the Save button again.");
-        }
-
-        private bool DepartmentExists(int id)
-        {
-          return _context.Departments.Any(e => e.DepartmentID == id);
+              + "was modified by another user after you. The "
+              + "edit operation was canceled and the current values in the database "
+              + "have been displayed. If you still want to edit this record, click "
+              + "the Save button again.");
         }
     }
 }
